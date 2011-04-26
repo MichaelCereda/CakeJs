858a7dab771906d13e1fcd4d8ecc7fbb48b5e2e6
Michael Cereda <michael.cereda@gmail.com>
2011-04-26 23:53:30 +0200

    deleted:    README deleted:    js/Cake/Animatable.js deleted:    js/Cake/Canvas.js 	deleted:    js/Cake/CanvasNode.js 	deleted:    js/Cake/CanvasSupport.js 	deleted:    js/Cake/Colors.js 	deleted:    js/Cake/Curves.js 	deleted:    js/Cake/Drawable.js 	deleted:    js/Cake/Drawables/CatmullRomSpline.js 	deleted:    js/Cake/Drawables/Circle.js 	deleted:    js/Cake/Drawables/Ellipse.js 	deleted:    js/Cake/Drawables/ImageNode.js 	deleted:    js/Cake/Drawables/Line.js 	deleted:    js/Cake/Drawables/Path.js 	deleted:    js/Cake/Drawables/Polygon.js 	deleted:    js/Cake/Drawables/Rectangle.js 	deleted:    js/Cake/Drawables/Spiral.js 	deleted:    js/Cake/Drawables/TextNode.js 	deleted:    js/Cake/E.js 	deleted:    js/Cake/ElementNode.js 	deleted:    js/Cake/Mouse.js 	deleted:    js/Cake/Pattern.js 	deleted:    js/Cake/RecordingContext.js 	deleted:    js/Cake/SVGParser.js 	deleted:    js/Cake/T.js 	deleted:    js/Cake/Timeline.js 	deleted:    js/Cake/Transformable.js 	deleted:    js/CakeJs.js 	deleted:    js/Utils/Array.js 	deleted:    js/Utils/Function.js 	deleted:    js/Utils/Gradient.js 	deleted:    js/Utils/Klass.js 	deleted:    js/Utils/Math.js 	deleted:    js/Utils/Object.js 	deleted:    js/Utils/String.js

--- js/Cake/T.js
+++
@@ -1,9 +0,0 @@
-/**
-  Shortcut for document.createTextNode.
-
-  @param {String} text The text for the text node
-  @return The created text node
-  */
-T = function(text) {
-  return document.createTextNode(text)
-}
